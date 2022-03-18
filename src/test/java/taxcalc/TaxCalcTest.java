27d020d4434df8b0a4024e3bf5cd7db6ddf29bb8
Krishan Shukla <krishan.shyam@gmail.com>
2022-03-18 15:37:58 +0000

    original code

---
+++ src/test/java/taxcalc/TaxCalcTest.java
@@ -0,0 +1,26 @@
+package taxcalc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class TaxCalcTest {
+
+    @Test
+    public void canCalculateTax() throws Exception {
+        Integer first = new TaxCalc(10).netAmount(new TaxCalc.Pair<>(40, "GBP"), new TaxCalc.Pair<>(50, "GBP"), new TaxCalc.Pair<>(60, "GBP")).first;
+        assertEquals(135, first.intValue());
+    }
+
+    @Test
+    public void cannotSumDifferentCurrencies() throws Exception {
+        try {
+            new TaxCalc(10).netAmount(new TaxCalc.Pair<>(4, "GBP"),
+                    new TaxCalc.Pair<>(5, "USD"));
+            fail("didn't throw");
+        } catch (Exception e) {
+
+        }
+    }
+}
